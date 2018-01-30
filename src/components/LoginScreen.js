10bd9d7b5a4e7ce008a5661bf13d88cf8670b882
Jang Jaehoon <idlinlee12@gmail.com>
2018-01-30 14:47:41 +0900

    LoginScreen에 style먹이기

--- src/components/LoginScreen.js
+++ src/components/LoginScreen.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react';
+import { Header, Grid, Button, Segment, Icon, Divider } from 'semantic-ui-react'
+import styled from 'styled-components';
+
+const FullHeightGrid = styled(Grid)`
+  height: 100vh;
+`
+
 
 export default class LoginScreen extends Component {
   render() {
     return (
-      <div>LoginScreen</div>
+      <FullHeightGrid centered verticalAlign="middle">
+        <Grid.Column style={{ width: '280px' }} textAlign='center'>
+          <Segment stacked padded="very">
+            <Divider horizontal inverted><Header size='huge'>로그인</Header></Divider>
+            <Button color='google plus' fluid>
+              <Icon name='google plus' /> Google Plus
+            </Button>
+            <Button color='twitter' fluid>
+              <Icon name='twitter' /> Twitter
+            </Button>
+          </Segment>
+        </Grid.Column>
+      </FullHeightGrid>
     );
   }
 }
