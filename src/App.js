a72aecacc487a5f8ef185a0c6c5554af39a1c5ed
Jang Jaehoon <idlinlee12@gmail.com>
2018-02-01 10:58:38 +0900

    root경로 접속시 Redirect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Redirect } from 'react-router-dom';
 import LoginScreenContainer from './containers/LoginScreenContainer';
 import ListPage from './containers/ListPage';
+import withAuth from './hocs/withAuth';
 
 
-
+const Home = withAuth(() => <Redirect to="/list" />);
 
 
 class App extends Component {
@@ -12,6 +13,7 @@ class App extends Component {
     return (
       <BrowserRouter>
         <div>
+          <Route path="/" exact component={Home} />
           <Route path="/login" component={LoginScreenContainer} />
           <Route path="/list" component={ListPage} />
         </div>
