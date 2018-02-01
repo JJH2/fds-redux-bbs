6897491ab51d2db77321086bcbe339439ec679af
Jang Jaehoon <idlinlee12@gmail.com>
2018-02-01 17:02:05 +0900

    BrowserRouter -> HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Redirect } from 'react-router-dom';
+import { HashRouter, Route, Redirect } from 'react-router-dom';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
@@ -20,13 +20,13 @@ class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <BrowserRouter>
+        <HashRouter>
           <div>
             <Route path="/" exact component={Home} />
             <Route path="/login" component={LoginScreenContainer} />
             <Route path="/list" component={ListPage} />
           </div>
-        </BrowserRouter>
+        </HashRouter>
       </Provider>
     );
   }
