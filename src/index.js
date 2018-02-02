62e76211708427a9ad2e1409d8c6ab59a7ecd395
Jang Jaehoon <idlinlee12@gmail.com>
2018-02-02 16:40:42 +0900

    .env.local 환경변수 설정

--- src/index.js
+++ src/index.js
@@ -7,12 +7,12 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 const config = {
-  apiKey: 'AIzaSyCyA2eYBGHMTWxOiBIP23Izr66Szr2tCUM',
-  authDomain: 'todolist1-82ee0.firebaseapp.com',
-  databaseURL: 'https://todolist1-82ee0.firebaseio.com',
-  projectId: 'todolist1-82ee0',
-  storageBucket: 'todolist1-82ee0.appspot.com',
-  messagingSenderId: '711342994667',
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
 };
 firebase.initializeApp(config);
 
