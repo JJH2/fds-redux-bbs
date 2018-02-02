9c3cf6053f078640b83cec5672ae8e581515cfd1
Jang Jaehoon <idlinlee12@gmail.com>
2018-02-02 11:49:25 +0900

    ArticleListContainer 버그수정

--- src/containers/articleListContainer.js
+++ src/containers/articleListContainer.js
@@ -1,7 +1,26 @@
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchArticleList } from '../ducts/articleList';
 import ArticleList from '../components/ArticleList';
 import withLoading from '../hocs/withLoading';
+
+const ArticleListWithLoading = withLoading(ArticleList);
+
+class ArticleListContainer extends Component {
+  static defaultProps = {
+    onMount: () => {},
+  };
+  componentDidMount() {
+    this.props.onMount();
+  }
+  render() {
+    // props중 onMount를 제외하고 내려준다. (...rest)
+    const { onMount, ...rest } = this.props;
+    return (
+      <ArticleListWithLoading {...rest} />
+    );
+  }
+}
 
 export default connect(
   // mapStateToProps
@@ -15,4 +34,4 @@ export default connect(
       dispatch(fetchArticleList());
     },
   }),
-)(withLoading(ArticleList));
+)(ArticleListContainer);
