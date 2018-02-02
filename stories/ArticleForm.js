bba2b320199948c9cf1c584d8bdd8d3b0db916c9
Jang Jaehoon <idlinlee12@gmail.com>
2018-02-02 14:08:13 +0900

    ArticleForm 작성

---
+++ stories/ArticleForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import ArticleForm from '../src/components/ArticleForm';
+
+storiesOf('ArticleForm', module)
+  .add('default', () => (
+    <ArticleForm />
+  ))
+  .add('error message', () => (
+    <ArticleForm onSubmit={action('onSubmit')} errorMessage="필드를 모두 채워야 합니다." />
+  ));
